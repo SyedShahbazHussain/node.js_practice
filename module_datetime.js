fbbd6b0d8ae42ccae5e4afe7b17cf18d08f68cf0
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2019-04-17 19:52:15 +0500

    Add

---
+++ module_datetime.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var dt = require('./module');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time is currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080);
