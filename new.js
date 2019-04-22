456df13313a56525107c0d156b4de16b5e91b931
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2019-04-22 11:19:37 +0500

    Commit

--- new.js
+++ new.js
@@ -1,16 +1,40 @@
 // var obj ={name: "Sha", age:"20"}
 // console.log(obj)
 
-function add (a,b){
-    return a+b;
+// function add (a,b){
+//     return a+b;
+// }
+
+// console.log(add(4,5));
+
+// //Anomious Function
+// var prints= function(){
+//     console.log("hello")
+// };
+// prints();
+
+// setTimeout(prints, 4000);
+
+function request(order) {
+    console.log("Customer order", order);
+    response(function (){
+        console.log("delivered data", order )
+    })
+}
+function response(callback){
+    setTimeout(callback, 4000)
 }
 
-console.log(add(4,5));
+request(1);
+request(2);
+request(3);
+request(4);
+request(5);
 
-//Anomious Function
-var prints= function(){
-    console.log("hello")
+var Name={
+    favcolor: "blue",
+    favmovie: "abc"
 };
-prints();
-
-setTimeout(prints, 4000);
+var New =Name;
+New.favcolor= "Green"
+console.log(Name.favcolor);
