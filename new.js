e5f7ae94b5b9e3cf9c813146af09a838cc65d67b
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2019-04-24 00:39:22 +0500

    Add

--- new.js
+++ new.js
@@ -15,26 +15,54 @@
 
 // setTimeout(prints, 4000);
 
-function request(order) {
-    console.log("Customer order", order);
-    response(function (){
-        console.log("delivered data", order )
-    })
-}
-function response(callback){
-    setTimeout(callback, 4000)
-}
+// function request(order) {
+//     console.log("Customer order", order);
+//     response(function (){
+//         console.log("delivered data", order )
+//     })
+// }
+// function response(callback){
+//     setTimeout(callback, 4000)
+// }
 
-request(1);
-request(2);
-request(3);
-request(4);
-request(5);
+// request(1);
+// request(2);
+// request(3);
+// request(4);
+// request(5);
 
-var Name={
-    favcolor: "blue",
-    favmovie: "abc"
+// var Name={
+//     favcolor: "blue",
+//     favmovie: "abc"
+// };
+// var New =Name;
+// New.favcolor= "Green"
+// console.log(Name.favcolor);
+
+// Difference b/t == & ===
+// console.log(20 == "20");
+
+// console.log(20 === "20");
+
+// var hello={
+//     printname: function(){
+//         console.log(this === hello)
+//     }
+// };
+// hello.printname();
+
+// function worthless(){
+//     console.log(this===global);
+// }
+// worthless()
+
+function person(name){
+    this.name=name
+}
+person.prototype={
+    my:function(){
+        console.log("My name is " + this.name)
+    }
 };
-var New =Name;
-New.favcolor= "Green"
-console.log(Name.favcolor);
+var fra = new person("Sha");
+fra.my()
